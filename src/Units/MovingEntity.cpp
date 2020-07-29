7668b36c968fc319fe67a8948a5e9b099ccd4998
nilkun <parkawaainilsson@gmail.com>
2020-07-30 00:35:29 +0900

    wrapping implemented, buggy when x = 0

--- src/Units/MovingEntity.cpp
+++ src/Units/MovingEntity.cpp
@@ -126,19 +126,83 @@ MovingEntity::MovingEntity() {
 	isNearBottom = false;
 
 };
-
+// SDL_Point* getNewPoints(SDL_Point[4] points, int x, int y) {	
+	// SDL_Point*  newPoints = new SDL_Point[4];
+		// *newPoints[0] =	{ points[0].x + x, points[0].y + y };
+		// *newPoints[1] = { points[1].x + x, points[1].y + y };
+		// *newPoints[2] =	{ points[2].x + x, points[2].y + y };
+		// *newPoints[3] =	{ points[3].x + x, points[3].y + y };
+	// }
+	// return newPoints;
+// };
+void setNewPoints(SDL_Point newPoints[4], SDL_Point points[4], int x, int y) {
+		newPoints[0].x = points[0].x + x;
+		newPoints[0].y = points[0].y + y;
+		newPoints[1].x = points[1].x + x;
+		newPoints[1].y = points[1].y + y;
+		newPoints[2].x = points[2].x + x;
+		newPoints[2].y = points[2].y + y; 
+		newPoints[3].x = points[3].x + x;
+		newPoints[3].y = points[3].y + y;
+};
 void MovingEntity::render(SDL_Renderer *renderer) {
+	int length = 15;
+	int radius = 5;
 	SDL_Point points[] = {
-		{ static_cast<int>(position.x + heading.x * 15), static_cast<int>(position.y + heading.y * 15) },
-		{ static_cast<int>(position.x + side.x * 5), static_cast<int>(position.y + side.y * 5) },
-		{ static_cast<int>(position.x - side.x * 5), static_cast<int>(position.y - side.y * 5 )},
-		{ static_cast<int>(position.x + heading.x * 15), static_cast<int>(position.y + heading.y * 15 )}
+		{ static_cast<int>(position.x + heading.x * length), static_cast<int>(position.y + heading.y * length) },
+		{ static_cast<int>(position.x + side.x * radius), static_cast<int>(position.y + side.y * radius) },
+		{ static_cast<int>(position.x - side.x * radius), static_cast<int>(position.y - side.y * radius )},
+		{ static_cast<int>(position.x + heading.x * length), static_cast<int>(position.y + heading.y * length )}
 	};
 	int count = 4; 
 	SDL_SetRenderDrawColor(renderer, 255, 128, 128, 255); // SET SCREEN TO BLACK
 	SDL_RenderDrawLines(renderer, points, count);
-};
+	
+SDL_Point extra[4];
+SDL_Point extra2[4];
+SDL_Point extra3[4];
 
+	if(position.x < length) {
+		setNewPoints(extra, points, 960, 0);
+		SDL_RenderDrawLines(renderer, extra, count);
+		if(position.y < length) {
+			setNewPoints(extra2, extra, 0, 540);
+			setNewPoints(extra3, points, 0, 540);
+			SDL_RenderDrawLines(renderer, extra2, count);
+			SDL_RenderDrawLines(renderer, extra3, count);
+		}
+		else if(position.y + length > 540) {
+			setNewPoints(extra2, extra, 0, -540);
+			setNewPoints(extra3, points, 0, -540);
+			SDL_RenderDrawLines(renderer, extra2, count);
+			SDL_RenderDrawLines(renderer, extra3, count);
+		}
+	}
+	else if(position.x + length > 960) {
+		setNewPoints(extra, points, -960, 0);
+		SDL_RenderDrawLines(renderer, extra, count);
+		if(position.y < length) {
+			setNewPoints(extra2, extra, 0, 540);
+			setNewPoints(extra3, points, 0, 540);
+			SDL_RenderDrawLines(renderer, extra2, count);
+			SDL_RenderDrawLines(renderer, extra3, count);
+		}
+		else if(position.y + length > 540) {
+			setNewPoints(extra2, extra, 0, -540);
+			setNewPoints(extra3, points, 0, -540);
+			SDL_RenderDrawLines(renderer, extra2, count);
+			SDL_RenderDrawLines(renderer, extra3, count);
+		}
+	}
+	else if(position.y < length) {
+			setNewPoints(extra, points, 0, 540);
+			SDL_RenderDrawLines(renderer, extra, count);
+		}
+		else if(position.y + length > 540) {
+			setNewPoints(extra, points, 0, -540);
+			SDL_RenderDrawLines(renderer, extra, count);
+		}
+};
 std::vector<Vehicle*> Vehicle::getNearby() {
 	std::vector<Vehicle*> nearby;
 	for(int i = 0; i < friends -> boids.size(); i++) {
@@ -166,8 +230,8 @@ std::vector<Vehicle*> Vehicle::getNearby() {
 fVector Vehicle::steering() {
 	fVector total = { 0, 0 };
 	float separationWeight = 8000;
-	float alignmentWeight = 0;//100;
-	float cohesionWeight = 0;//50;
+	float alignmentWeight = 100;
+	float cohesionWeight = 50;
 
 	std::vector<Vehicle*> close = getNearby();
 	if(close.size() < 2 ) return total;
@@ -176,22 +240,26 @@ fVector Vehicle::steering() {
 	fVector separation{0, 0};
 	fVector alignment{0, 0};
 	fVector cohesion{0, 0};
-	fVector *own;
+	fVector *pos;
 	for(int i = 0; i < close.size(); i++) {
 		
 		Vehicle *current = close[i];
-		own = 
+		pos = 
 			current -> isNearBottom 
-			? &current -> extraPos
-			: &current -> position;
-
+			? &close[i] -> extraPos
+			: &close[i] -> position;
+		fVector test1 = { 
+			(abs(position.x - current -> position.x) > 100) ? current -> extraPos.x : current -> position.x,
+			(abs(position.y - current -> position.y) > 100) ? current -> extraPos.y : current -> position.y
+		};
+		
 		if(current == this) continue;
-		fVector distance = position - *own;
+		fVector distance = position - test1;
 
 		// separation += distance.getNormalized() / distance.length();
 		separation += distance.getNormalized() / distance.length();
 		alignment += current -> heading;
-		cohesion += *own;
+		cohesion += test1;
 	}
 	// std::cout << separation.x << " x " << alignment.x << " x " << cohesion.x << std::endl;
 	// cohesion = (cohesion / close.size() - this -> position ).getNormalized() ;
@@ -223,35 +291,40 @@ void Vehicle::update(float elapsed_time) {
 	velocity.truncate(maxSpeed);
 	position += velocity * elapsed_time;
 //
-	isNearBottom = false;
-	if(position.x < 100) {
-		extraPos.x = 960 + position.x;
-		extraPos.y = position.y;
-	}	
-	if(position.x < 100 || position.x > 860) {
-		extraPos.x = position.x - 960;
-		if 
-		isNearBottom = true;
-		if(position.y > 440 || position.y < 100) 
-		{ 
-			extraPos.y = 540 - position.y;
-		}
-		else extraPos.y = position.y;
-	}	
-	else if(position.y > 440 || position.y < 100) 
-	{ 
-		extraPos.x = position.x;
-		extraPos.y = 540 - position.y;
-		isNearBottom = true;
 
-	}
 	if(position.x > 960) position.x -= 960;
 	else if (position.x < 0) position.x += 960;
     if(position.y > 540) position.y -= 540;
 	else if (position.y <0) position.y += 540;	
+
 	if(velocity.lengthSq() > 0.00000001)
 	{
 		heading = velocity.getNormalized();
 		side = heading.getPerp();
+	}
+	
+	// Find close in torus
+	isNearBottom = false;
+	if(position.x < 100) {
+		extraPos.x = 960 + position.x;
+		extraPos.y = position.y;
+		isNearBottom = true;
+	}	
+	else if(position.x > 860) {
+		extraPos.x = position.x - 960;
+		isNearBottom = true;
+		extraPos.y = position.y;
+	}	
+	if(position.y > 440) 
+	{ 
+		if(!isNearBottom) extraPos.x = position.x;
+		isNearBottom = true;
+		extraPos.y = position.y - 540;
+	}
+	if(position.y < 100) 
+	{ 
+		if(!isNearBottom) extraPos.x = position.x;
+		extraPos.y = 540 + position.y;
+		isNearBottom = true;
 	}
 }
